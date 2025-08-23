984fdc9b30fc55362da8d6498262b80c28fab43a
Seth Sullivan <sethsull@udel.edu>
2025-08-23 11:55:48 -0400

    update getEvents import

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -3,7 +3,7 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
-import { getEvents } from "@utils/getCalendarEvents";
+import { getEvents } from "@/lib/utils/getCalendarEvents";
 
 interface AvailabilitySlot {
     id: string;
