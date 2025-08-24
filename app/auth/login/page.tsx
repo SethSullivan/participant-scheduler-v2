82ea7bd75c128c21fb68413e3b51fc5fd9832db8
Seth Sullivan <sethsull@udel.edu>
2025-08-24 15:44:52 -0400

    anonymous sign in to continue without an account

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,11 +1,31 @@
+'use client'
 import { LoginForm } from "@/components/login-form";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { createClient } from "@/lib/supabase/client";
 
 export default function Page() {
-  return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <LoginForm />
-      </div>
-    </div>
-  );
+	const router = useRouter();
+
+	const handleAnonymousAccess = async () => {
+    const supabase = createClient();
+    const { data, error } = await supabase.auth.signInAnonymously()
+    if (error) throw error;
+		console.log("Navigating to /protected");
+		router.push("/protected");
+	};
+
+	return (
+		<div className="flex items-center justify-center min-h-screen p-6">
+			<div className="w-full max-w-sm space-y-6">
+				<LoginForm />
+				<div className="pt-[1px]">
+					<Button className="w-full" variant="outline" onClick={handleAnonymousAccess}>
+						Continue Without An Account
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
 }
