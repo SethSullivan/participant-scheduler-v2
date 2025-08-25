abd1f4c7c0229948768381bafad013d12a4a418c
Seth Sullivan <sethsull@udel.edu>
2025-08-25 14:56:00 -0400

    link to dashboard page now

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <main className="flex-1 flex flex-col gap-6 px-4">
             <h2 className="font-medium text-xl mb-4">Scheduling participants with ease</h2>
-              <Link href={"/protected"}>Get Started</Link>
+              <Link href={"/protected/dashboard"}>Get Started</Link>
           </main>
         </div>
 
