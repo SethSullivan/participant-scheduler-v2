54feb6495f35309ff86ccc4b1068fc364ac6f7f9
Seth Sullivan <sethsull@udel.edu>
2025-08-28 09:27:42 -0400

    useAvailabilityData hook

---
+++ hooks/useAvailabilityData.ts
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react';
+import { createClient } from "@/lib/supabase/client";
+import { useAuth } from './useAuth';
+type AvailabilitySlot = {
+	id: string;
+	title: string;
+	start: Date;
+	end: Date;
+	isGcal: boolean;
+	backgroundColor?: string;
+	borderColor?: string;
+	textColor?: string;
+}
+type AvailabilityData = {
+    id:string;
+    created_at:Date;
+    availability:AvailabilitySlot[];
+    eventID:string;
+}
+export default function useAvailabilityData(organizerID:string|undefined, eventID:string|undefined){
+    const [availabilityData, setAvailabilityData] = useState<AvailabilityData[]|null>(null);
+    const authData = useAuth();
+    const userID = authData?.claims.sub
+    useEffect(() => {
+        const getParticipantAvailability = async () => {
+            console.log("userID", userID) 
+            console.log("organizerID",organizerID)
+            // If there's no userID, this user shouldn't be able to see others availability
+            if (!userID) {
+                return 
+            }
+
+            // If the current user is not the organizer, then they shouldn't be able to see others availability
+            if (userID != organizerID) {
+                return
+            }
+
+            const supabase = createClient();
+			try {
+				const { error: availabilityError, data: availabilityResponse } = await supabase
+					.from("participant_availability")
+					.select("*")
+					.eq("eventID", eventID);
+                console.log("availability Response", availabilityResponse)
+				if (availabilityError) {
+					throw availabilityError;
+				}
+                setAvailabilityData(availabilityResponse); 
+
+			} catch (error) {
+				console.error(error)
+			}
+		};
+        getParticipantAvailability();
+    }, [eventID, organizerID, userID])
+    return availabilityData
+}
