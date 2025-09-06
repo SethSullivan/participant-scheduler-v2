edb28a84487a5b79f0d8204174e4e52c497b29e6
Seth Sullivan <sethsull@udel.edu>
2025-09-06 13:45:39 -0400

    navbar spacing aligns with content

--- app/[pageID]/layout.tsx
+++ app/[pageID]/layout.tsx
@@ -1,5 +1,5 @@
 import { AuthButton } from "@/components/auth-button";
-import { ThemeSwitcher } from "@/components/theme-switcher";
+// import { ThemeSwitcher } from "@/components/theme-switcher";
 import Link from "next/link";
 
 export default function ProtectedLayout({
@@ -11,8 +11,8 @@ export default function ProtectedLayout({
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col items-center">
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-            <div className="flex gap-5 items-center font-semibold">
+          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-1 text-sm">
+            <div className="flex items-center font-semibold">
               <Link href={"/"}>Participant Scheduler</Link>
             </div>
             <AuthButton />
