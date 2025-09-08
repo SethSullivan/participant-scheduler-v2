886904125614aafe29715511ed0a6039c5070a04
Seth Sullivan <sethsull@udel.edu>
2025-09-08 14:53:55 -0400

    rotate colors for up to 10 participants

--- lib/utils/utils.ts
+++ lib/utils/utils.ts
@@ -7,14 +7,14 @@ export function cn(...inputs: ClassValue[]) {
 
 // Color wheel
 export const colors = [
-  "bg-red-500",
-  "bg-green-500",
-  "bg-blue-500",
-  "bg-yellow-500",
-  "bg-purple-500",
-  "bg-pink-500",
-  "bg-indigo-500",
-  "bg-teal-500",
-  "bg-orange-500",
-  "bg-cyan-500",
+  "rgb(239, 68, 68)", // bg-red-500
+  "rgb(22, 163, 74)", // bg-green-500
+  "rgb(37, 99, 235)", // bg-blue-500
+  "rgb(250, 204, 21)", // bg-yellow-500
+  "rgb(128, 0, 128)", // bg-purple-500
+  "rgb(236, 72, 153)", // bg-pink-500
+  "rgb(75, 0, 130)", // bg-indigo-500
+  "rgb(0, 128, 128)", // bg-teal-500
+  "rgb(255, 165, 0)", // bg-orange-500
+  "rgb(0, 255, 255)", // bg-cyan-500
 ];
