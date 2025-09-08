476eb73b5067e80b032ae8313fe9f0f0471b6c9b
Seth Sullivan <sethsull@udel.edu>
2025-09-08 14:31:28 -0400

    added colors to utils

--- lib/utils/utils.ts
+++ lib/utils/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// Color wheel
+export const colors = [
+  "bg-red-500",
+  "bg-green-500",
+  "bg-blue-500",
+  "bg-yellow-500",
+  "bg-purple-500",
+  "bg-pink-500",
+  "bg-indigo-500",
+  "bg-teal-500",
+  "bg-orange-500",
+  "bg-cyan-500",
+];
