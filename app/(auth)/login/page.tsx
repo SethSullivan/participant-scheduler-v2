9ebd5f274f868f50bcc4dac7c0a018b554879e42
Seth Sullivan <sethsull@udel.edu>
2025-09-13 09:22:17 -0400

    popup button for login with email added, to encourage a google login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,19 +1,37 @@
 "use client";
 import { GoogleLoginButton } from "@/components/google-login-button";
 import { LoginForm } from "@/components/login-form";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 export default function Page() {
+  const [showEmailLogin, setShowEmailLogin] = useState(false);
   return (
-    <div className="flex flex-col items-center justify-center">
-      <div className="flex items-center justify-center border-2">
-        <div className="w-full max-w-sm">
+    <div className="flex flex-col h-screen items-center justify-center">
+      <div className="flex-col w-full flex items-center justify-center">
+        <div className="w-full max-w-sm text-center text-md">
+          For Google Calendar integration (recommended)
+          <GoogleLoginButton/>
+        </div>
+        {!showEmailLogin &&
+        <div className="w-full max-w-sm pt-3 text-center">
+           - or -
+        </div>
+        }
+        {!showEmailLogin &&
+        <div className="flex w-full max-w-sm pt-3 items-center justify-center">
+           <Button onClick={() => setShowEmailLogin(true)}>
+              Login with Email
+           </Button>
+        </div>
+        }
+      </div>
+      {showEmailLogin  &&
+      <div className="flex pt-6">
+        <div className="flex w-full max-w-sm">
           <LoginForm />
         </div>
       </div>
-      <div className="flex items-center justify-center">
-        <div className="w-full max-w-sm">
-          <GoogleLoginButton/>
-        </div>
-      </div>
+      }
     </div>
   );
 }
