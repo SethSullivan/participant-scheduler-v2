7dd03cc2894048df5589e22ebfb32faf449f3c42
Seth Sullivan <sethsull@udel.edu>
2025-11-05 12:46:43 -0500

    update props for participantItem

--- __tests__/CalendarSideBar-test.tsx
+++ __tests__/CalendarSideBar-test.tsx
@@ -25,6 +25,7 @@ describe("CalendarSideBar", () => {
 });
 
 const mockHandleCheckUpdate = jest.fn();
+const mockHandleDeleteParticipant = jest.fn();
 describe("ParticipantItem", () => {
   describe("Renders participant info correctly", () => {
     it("Should have a checkbox, display name, and email below name", () => {
@@ -33,6 +34,7 @@ describe("ParticipantItem", () => {
           idx={0}
           participant={mockParticipantInfo[0]}
           handleCheckUpdate={mockHandleCheckUpdate}
+          handleDeleteParticipant={mockHandleDeleteParticipant}
         />
       );
       expect(screen.getByRole("checkbox")).toBeInTheDocument();
@@ -45,6 +47,7 @@ describe("ParticipantItem", () => {
           idx={0}
           participant={mockParticipantInfo[0]}
           handleCheckUpdate={mockHandleCheckUpdate}
+          handleDeleteParticipant={mockHandleDeleteParticipant}
         />
       );
       expect(screen.getByLabelText("Delete Participant")).toBeInTheDocument();
