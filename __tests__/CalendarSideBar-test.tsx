f4a79e4d66630bdd1627a6cccf473d32072d8af9
Seth Sullivan <sethsull@udel.edu>
2025-11-05 12:45:08 -0500

    tests for sidebar

---
+++ __tests__/CalendarSideBar-test.tsx
@@ -0,0 +1,53 @@
+import { screen, render } from "@testing-library/react";
+import CalendarSideBar, {
+  ParticipantItem,
+} from "@/components/calendar-sidebar";
+
+const mockParticipantInfo = [
+  {
+    userID: "user1",
+    name: "John Doe (john@example.com)",
+    color: "#FF0000",
+    isChecked: true,
+  },
+  {
+    userID: "user2",
+    name: "Jane Smith (jane@example.com)",
+    color: "#00FF00",
+    isChecked: false,
+  },
+];
+
+describe("CalendarSideBar", () => {
+  it("renders correctly", () => {
+    test.todo;
+  });
+});
+
+const mockHandleCheckUpdate = jest.fn();
+describe("ParticipantItem", () => {
+  describe("Renders participant info correctly", () => {
+    it("Should have a checkbox, display name, and email below name", () => {
+      render(
+        <ParticipantItem
+          idx={0}
+          participant={mockParticipantInfo[0]}
+          handleCheckUpdate={mockHandleCheckUpdate}
+        />
+      );
+      expect(screen.getByRole("checkbox")).toBeInTheDocument();
+      expect(screen.getByText("John Doe")).toBeInTheDocument();
+      expect(screen.getByText("john@example.com")).toBeInTheDocument();
+    });
+    it("should display a garbage can indicator for deleting", () => {
+      render(
+        <ParticipantItem
+          idx={0}
+          participant={mockParticipantInfo[0]}
+          handleCheckUpdate={mockHandleCheckUpdate}
+        />
+      );
+      expect(screen.getByLabelText("Delete Participant")).toBeInTheDocument();
+    });
+  });
+});
